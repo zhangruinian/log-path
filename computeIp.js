8d601f758ec082c8dde20c30e9c0cab4d50964be
“frontIT” <zhangruinian3@gami.com>
2019-04-21 14:24:41 +0800

    修改统计ip

--- computeIp.js
+++ computeIp.js
@@ -2,8 +2,8 @@ const readline = require('readline')
 const path = require('path')
 const fs = require('fs')
 // let filepath = path.join(__dirname, 'path.log')
-// let filepath = path.join(__dirname, 'gov-paths.log')
-let filepath = path.join(__dirname, '清洗-数据.log')
+let filepath = path.join(__dirname, 'gov-paths.log')
+// let filepath = path.join(__dirname, '清洗-数据.log')
 let input = fs.createReadStream(filepath)
 const rl = readline.createInterface({
     input: input
@@ -21,7 +21,7 @@ rl.on('close', (line) => {
     console.log(ipArr.length)
     num = new Set(ipArr).size
     console.log('ip个数', num)
-    /*fs.writeFile('ip个数.csv', JSON.stringify(ipArr), function (error) {
+   /* fs.writeFile('ip个数.csv', JSON.stringify(ipArr), function (error) {
         error ? console.log(error) : console.log('写入成功')
     })*/
 })
